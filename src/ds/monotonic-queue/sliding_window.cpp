fe24ba2a59843375d61b210432b24a8124b01c8a
lnkkerst <lnkkerst04@gmail.com>
2023-09-30 20:45:43 +0800

    feat: add monotonic-stack,monotonic-queue,binary-indexed-tree

---
+++ src/ds/monotonic-queue/sliding_window.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize(2)
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  for (auto &i : a) {
+    cin >> i;
+  }
+  // 滑动窗口最值
+  auto calc = [&](function<bool(int, int)> cmp) {
+    deque<int> q;
+    for (int i = 0; i < k; ++i) {
+      while (!q.empty() && cmp(a[i], a[q.back()])) {
+        q.pop_back();
+      }
+      q.push_back(i);
+    }
+    cout << a[q.front()] << ' ';
+    for (int i = k; i < n; ++i) {
+      while (!q.empty() && cmp(a[i], a[q.back()])) {
+        q.pop_back();
+      }
+      q.push_back(i);
+      while (q.front() <= i - k) {
+        q.pop_front();
+      }
+      cout << a[q.front()] << ' ';
+    }
+    cout << endl;
+  };
+  calc(less_equal<>());    // 最小值
+  calc(greater_equal<>()); // 最大值
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  t = 1;
+  while (t--) {
+    solve();
+  }
+}
---
+++ src/ds/monotonic-stack/README.md
@@ -0,0 +1,7 @@
+# 单调栈
+
+求元素右侧第一个大于他的元素的下标（[Luogu-P5788](https://www.luogu.com.cn/problem/P5788)）。
+
+```cpp
+{{ #include main.cpp:17:36 }}
+```
